8ba5dea3f720e9359bc7db6c67895733eccbdbcb
karansaggu2002 <karansaggu001.com>
2024-11-12 10:34:01 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Myapp from './components/Myapp';
+
+function App() {
+  return (
+    <>
+    <Myapp />
+    </>
+  );
+}
+
+export default App;
